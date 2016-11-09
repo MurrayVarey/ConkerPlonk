ba6369837cf12cf9ed86c4c83f875cb67a89c7f3
MurrayVarey <Murray.Varey@gmail.com>
2016-11-09 21:13:46 +0000

    Adds Load Image button.

--- app/src/main/java/murrayvarey/conkerplonk/MainActivity.java
+++ app/src/main/java/murrayvarey/conkerplonk/MainActivity.java
@@ -1,13 +1,29 @@
 package murrayvarey.conkerplonk;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int RESULT_LOAD_IMAGE = 1;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLoadPictureClick(View view)
+    {
+        Intent getFromGalleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(getFromGalleryIntent, RESULT_LOAD_IMAGE);
+    }
+
+
 }
